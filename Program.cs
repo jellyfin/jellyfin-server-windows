cf94c3d18a233988d769dce91fa1e46537443451
Froghut <abomb@abomb.de>
2019-10-14 23:28:08 +0200

    - read registry to get jellyfin paths - make app request admin privileges to start/stop service

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using JellyfinTray.Properties;
+using Microsoft.Win32;
 
 namespace JellyfinTray
 {
@@ -30,55 +31,62 @@ namespace JellyfinTray
 	public class TrayApplicationContext : ApplicationContext
 	{
 		private readonly string _jellyfinServiceName = "JellyfinServer";
+		private string _executableFile;
 		private string _dataFolder = @"C:\ProgramData\Jellyfin\Server";
 		private string _localJellyfinUrl = "http://localhost:8096";
 		private NotifyIcon _trayIcon;
 		private ServiceController _serviceController;
-		private MenuItem _menuItemService;
 		private MenuItem _menuItemStart;
 		private MenuItem _menuItemStop;
 		private MenuItem _menuItemOpen;
 		private MenuItem _menuItemExit;
+		private string _installFolder;
 
 		public TrayApplicationContext ()
 		{
 			_serviceController = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == _jellyfinServiceName);
 
-			if (_serviceController != null)
+			try
+			{
+				RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("Software\\Jellyfin\\Server");
+				// registry keys are probably written by a 32bit installer, so check the 32bit registry folder
+				if (registryKey == null)
+					registryKey = Registry.LocalMachine.OpenSubKey("Software\\WOW6432Node\\Jellyfin\\Server");
+				_installFolder = registryKey.GetValue("InstallFolder").ToString();
+				_dataFolder = registryKey.GetValue("DataFolder").ToString();
+				XDocument systemXml = XDocument.Load(Path.Combine(_dataFolder, "config\\system.xml"));
+				string port = systemXml.CreateNavigator().SelectSingleNode("/ServerConfiguration/PublicPort").Value;
+				_localJellyfinUrl = "http://localhost:" + port;
+			}
+			catch(Exception ex)
+			{
+				// We could not get the Jellyfin port from system.xml config file - just use default?
+			}
+
+			
+			if (_serviceController == null)
 			{
 				try
 				{
-					ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Service");
-					ManagementObjectCollection collection = searcher.Get();
-					ManagementObject managementObject = collection.OfType<ManagementObject>().First(o => o["Name"] as string == _jellyfinServiceName);
-					string pathName = managementObject["PathName"] as string;
-					string appParameters = StartGetOutput(pathName, $"get {_jellyfinServiceName} AppParameters");
-					Regex dataDirRegex = new Regex("--datadir \"(.*?)\"");
-					Match match = dataDirRegex.Match(appParameters);
-					if (match.Success)
-					{
-						_dataFolder = match.Groups[1].Value;
-					}
-
-					XDocument systemXml = XDocument.Load(Path.Combine(_dataFolder, "config\\system.xml"));
-					string port = systemXml.CreateNavigator().SelectSingleNode("/ServerConfiguration/PublicPort").Value;
-					_localJellyfinUrl = "http://localhost:" + port;
+					_executableFile = Path.Combine(_installFolder, "jellyfin.exe");
+					if (!File.Exists(_executableFile))
+						throw new FileNotFoundException();
 				}
-				catch(Exception ex)
+				catch
 				{
-					// We could not get the jellyfin port from system.xml config file - just use default?
+					// We could not find the Jellyfin executable file!
+					MessageBox.Show("Could not find a Jellyfin Installation.");
+					Application.Exit();
+					return;
 				}
 			}
-
-			_menuItemService = new MenuItem("Jellyfin Service not found!");
-			_menuItemService.Enabled = false;
+			
 			_menuItemStart = new MenuItem("Start Jellyfin", Start);
 			_menuItemStop = new MenuItem("Stop Jellyfin", Stop);
 			_menuItemOpen = new MenuItem("Open Jellyfin", Open);
 			_menuItemExit = new MenuItem("Exit", Exit);
 
-			ContextMenu contextMenu = new ContextMenu(new MenuItem[] {
-																		 _menuItemService,
+			ContextMenu contextMenu = new ContextMenu(new[] {
 				                                                         _menuItemStart,
 				                                                         _menuItemStop,
 				                                                         new MenuItem("-"), 
@@ -103,9 +111,13 @@ namespace JellyfinTray
 		private void ContextMenuOnPopup(object sender, EventArgs e)
 		{
 			bool serviceExists = _serviceController != null;
-			_menuItemService.Visible = !serviceExists;
-			bool running = serviceExists && _serviceController.Status == ServiceControllerStatus.Running;
-			bool stopped = serviceExists && _serviceController.Status == ServiceControllerStatus.Stopped;
+			bool exeRunning = false;
+			if (serviceExists)
+				_serviceController.Refresh();
+			else
+				exeRunning = Process.GetProcessesByName("jellyfin").Length > 0;
+			bool running = (!serviceExists && exeRunning) || (serviceExists && _serviceController.Status == ServiceControllerStatus.Running);
+			bool stopped = (!serviceExists && !exeRunning) || (serviceExists && _serviceController.Status == ServiceControllerStatus.Stopped);
 			_menuItemStart.Enabled = stopped;
 			_menuItemStop.Enabled = running;
 			_menuItemOpen.Enabled = running;
@@ -113,12 +125,24 @@ namespace JellyfinTray
 
 		private void Start(object sender, EventArgs e)
 		{
-			_serviceController.Start();
+			if (_serviceController != null)
+				_serviceController.Start();
+			else
+				Process.Start(_executableFile);
 		}
 
 		private void Stop(object sender, EventArgs e)
 		{
-			_serviceController.Stop();
+			if (_serviceController != null)
+				_serviceController.Stop();
+			else
+			{
+				Process process = Process.GetProcessesByName("jellyfin").FirstOrDefault();
+				if (process == null)
+					return;
+				if (process.CloseMainWindow())
+					process.Kill();
+			}
 		}
 
 		void Exit(object sender, EventArgs e)
@@ -134,14 +158,17 @@ namespace JellyfinTray
 			process.StartInfo.Arguments = arguments;
 			process.StartInfo.UseShellExecute = false;
 			process.StartInfo.RedirectStandardOutput = true;
-			process.StartInfo.CreateNoWindow = true;
+			process.StartInfo.RedirectStandardError = true;
+			process.StartInfo.CreateNoWindow = false;
 			process.Start();
 
 			string output = process.StandardOutput.ReadToEnd();
+			output += Environment.NewLine + process.StandardError.ReadToEnd();
 
 			process.WaitForExit();
 
 			return output;
 		}
+
 	}
 }
